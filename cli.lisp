87a371bb61ddd5e499d15d51900070fa633a08b2
ellis <ellis@rwest.io>
2023-10-07 00:10:50 -0400

    cli bump

--- cli.lisp
+++ cli.lisp
@@ -2,7 +2,7 @@
 
 ;;; Code:
 (defpackage skel.cli
-  (:use :cl :cond :cli :skel :fmt :log)
+  (:use :cl :cond :cli :skel :fmt :log :fu)
   (:import-from :sb-posix :getcwd)
   (:export :main))
 
@@ -13,11 +13,11 @@
   :version "0.1.1"
   :description "A hacker's project compiler and build tool."
   :opts (make-opts 
-	  (:name help :description "print this message")
-	  (:name version :description "print version")
+	  (:name help :global t :description "print this message" :thunk (lambda (x) (declare (ignore x)) (print-help $cli)))
+	  (:name version :global t :description "print version" :thunk (lambda (x) (declare (ignore x)) (print-version $cli)))
 	  (:name debug 
 	   :global t :description "set log level (debug,info,trace,warn)"
-	   :thunk (lambda (&optional x) (setq *log-level* x)))
+	   :thunk (lambda (x) (or (when x :debug) *log-level*)))
 	  (:name input :description "input source")
 	  (:name output :description "output target"))
   :cmds (make-cmds
@@ -30,7 +30,8 @@
 		   (:name kind :description "project kind")))
 	  (:name describe
 	   :description "describe the project skelfile"
-	   :opts (make-opts (:name file :description "path to skelfile")))
+	   :opts (make-opts (:name file :description "path to skelfile"))
+	   :thunk (lambda (x) (declare (ignore x)) (describe (find-skelfile #P"." :load t))))
 	  (:name build
 	   :description "build artifacts"
 	   :opts (make-opts (:name target :description "target artifact to build")))
@@ -38,16 +39,15 @@
 
 (defun run ()
   (with-cli (opts cmds) $cli
-    (loop for o in (active-opts $cli t)
-	  do (do-opt o))
+    (loop for o across (active-opts $cli t)
+	  if (string= "debug" (cli-name o))
+	    do (setq *log-level* (do-opt o))
+	  else do (do-opt o))
     (debug! (cli-opts $cli) (cli-cmd-args $cli) (cli-cmds $cli))
-    (cond
-      ((member "build" *argv* :test #'string=) (nyi!))
-      ((member "describe" *argv* :test #'string=) (describe (find-skelfile #P"." :load t)))
-      ((member "-h" *argv* :test #'string=) (print-help $cli))
-      ((member "-v" *argv* :test #'string=) (print-version $cli))
-      ((member "-f" *argv* :test #'string=) (nyi!))
-      (t (print-help $cli)))))
+    (loop for c across (active-cmds $cli)
+	  if (string= "describe" (cli-name c))
+	    do (do-cmd c)
+	  else do (iprintln ":OK"))))
        
 (defmain ()
   (run)
